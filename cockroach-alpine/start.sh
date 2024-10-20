e69ce226fa06a7b6e3b0d07dd9970f4b7c71ff88
yid0 <idoughiyani@gmail.com>
2024-10-20 12:15:29 +0200

    add cockroachDB based on alpine + update glibcpine

---
+++ cockroach-alpine/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+START="start-single-node --insecure"
+JOIN_CLUSTER=
+
+if [ "$COCKROACH_ENV" = "production" ]; then
+    $START=start
+    $JOIN_CLUSTER=--join=$COCKROACH_NODES
+    --certs-dir=/var/lib/cockroachdb/certs
+fi
+
+cockroach $START --http-addr="$COCKROACH_HOST":"$COCKROACH_HTTP_PORT" --listen-addr="$COCKROACH_HOSTNAME":"$COCKROACH_PORT" $JOIN_CLUSTER
