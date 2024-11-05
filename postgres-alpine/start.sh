ef3eba1c6e9faa871777b77b99518bda05972928
yid0 <idoughiyani@gmail.com>
2024-11-05 13:38:09 +0100

    fix ci/cid

--- postgres-alpine/start.sh
+++ postgres-alpine/start.sh
@@ -6,8 +6,6 @@ set -e
 if [ -z "$(ls -A $PGDATA)" ]; then
   echo "Initializing new database..."
   initdb -D $PGDATA
-  echo "Creates certs and configure SSL ..."
-  ssl.sh
 else
   echo "Database already exists, skipping initdb..."
 fi
@@ -18,6 +16,12 @@ if [ "$POSTGRES_ENV" = "dev" ]; then
   echo "host    all             all             0.0.0.0/0          md5" >> $PGDATA/pg_hba.conf
 
 fi;
+
+echo "Creates certs and configure SSL ..."
+
+ssl.sh
+
+echo "hostssl    all             all             0.0.0.0/0          md5" >> $PGDATA/pg_hba.conf
 
 sed -i "s|^#logging_collector = off|logging_collector = on|" $PGDATA/postgresql.conf
 sed -i "s|^#log_directory =.*|log_directory = '/var/lib/postgresql/logs'|" $PGDATA/postgresql.conf
@@ -34,4 +38,6 @@ pg_ctl reload
 pg_ctl -D "$PGDATA" -m fast stop
 
 echo "Starting postgressql ... "
+
 exec pg_ctl -D $PGDATA start -l $PGLOG/app.log -o "-c config_file=$PGDATA/postgresql.conf"
+# postgres -D "$PGDATA" -c config_file=$PGDATA/postgresql.conf
